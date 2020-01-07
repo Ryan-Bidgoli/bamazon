b0c6f05c364bc8dd76b9c8eb69c4f1ec9e5e189e
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-01-06 22:42:31 -0800

    process for another purchase

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -84,4 +84,18 @@ connection.query('SELECT * FROM Products', function(err, res){
     })
 })
 }
- 
+ function reprompt(){
+  inquirer.prompt([{
+    type: "confirm",
+    name: "reply",
+    message: "Would you like to purchase another item?"
+  }]).then(function(ans){
+    if(ans.reply){
+      start();
+    } else{
+      console.log("See you soon!");
+    }
+  });
+}
+
+start(); 
