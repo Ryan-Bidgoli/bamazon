dd6d89a0f5a139ef92840cd01be925560e37b27e
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-01-07 01:44:28 -0800

    create or view department

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -8,3 +8,21 @@ var connection = mysql.createConnection({
   password: "",
   database: "Bamazon"
 })
+
+
+function start(){
+  inquirer.prompt([{
+    type: "list",
+    name: "doThing",
+    message: "What would you like to do?",
+    choices: ["View Product Sales by Department", "Create New Department", "End Session"]
+  }]).then(function(ans){
+    switch(ans.doThing){
+      case "View Product Sales by Department": viewProductByDept();
+      break;
+      case "Create New Department": createNewDept();
+      break;
+      case "End Session": console.log('Bye!');
+    }
+  });
+}
