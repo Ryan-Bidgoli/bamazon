e851ea8098414f93a9a7d13b29f69db3dc7ee810
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-01-07 02:04:32 -0800

    various table products

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE Products(
+    ItemID MEDIUMINT AUTO_INCREMENT NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    StockQuantity INT(10) NOT NULL,
+    primary key(ItemID)
+);
