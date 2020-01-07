c9a811af1c56875d559a7c47ba253ce5bc32c595
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-01-07 02:07:41 -0800

    table of departments

--- schema.sql
+++ schema.sql
@@ -25,3 +25,11 @@ VALUES ("Uncharted 4","ENTERTAINMENT",49.95,150),
     ("Mad Max: Fury Road","ENTERTAINMENT",25.50,57),
     ("Monopoly","ENTERTAINMENT",30.50,35),
     ("Yahtzee","ENTERTAINMENT",19.95,23);
+
+
+CREATE TABLE Departments(
+    DepartmentID MEDIUMINT AUTO_INCREMENT NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    OverHeadCosts DECIMAL(10,2) NOT NULL,
+    TotalSales DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY(DepartmentID));
